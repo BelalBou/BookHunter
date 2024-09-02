0e84f91b9c7e9284bf4905bc3e0ee0d3099a1132
BelalBou <belalbou48@gmail.com>
2024-09-02 12:58:14 +0200

    Initial Commit

---
+++ app/config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+
+// INITIALISATION DES ZONES DYNAMIQUES
+$title = "";
+$content = "";
+
+// PARAMÈTRES DE CONNEXION
+define("DB_HOST", "localhost:3306");
+define("DB_NAME", "book_hunter_2024");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
